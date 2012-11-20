60a988eb2060f67cae2f77afea90cc94bb73f501
wbr <wbr76@hanmail.net>
2012-11-20 17:32:31 +0900

    -

--- js/lofunc.js
+++ js/lofunc.js
@@ -4,14 +4,8 @@ var totcnt;
 var imgNum;
 var resultList;
 
-
-
-function init() {
-	moveMain = setInterval('goMain()', 1000);
-}
 function goMain() {
-	$.mobile.changePage("#main" ) ;
-	//clearInterval(moveMain);
+	$.mobile.changePage("#main") ;  //, "pop", false,false
 }
 
 $(function(){
@@ -49,11 +43,13 @@ $(function(){
 	$ ("#aHomeOrgDetail").bind( "tap" , function(event){
 		goMain()
 	});	
+	$ ("#aHomeOrgDetailF").bind( "tap" , function(event){
+		goMain()
+	});	
 	$ ("#aListOrgDetail").bind( "tap" , function(event){
 		$.mobile.changePage("#org_list" ) ;
 	});	
-	
-	
+
 });
 
 
@@ -76,19 +72,26 @@ function getTypeList(type){
 		}
 		else{
 			var grpValue = "";
+			var idxChk =0;
 			for (var i = 0; i < totcnt; i++) {
 				
 				var result = resultList[i];
 				if(grpValue == "" || grpValue != result.grp_value ){
-					list_html += "<li data-role=\"list-divider\"  role=\"heading\" class=\"ui-li ui-li-divider ui-btn ui-bar-b ui-corner-top ui-btn-up-undefined\" >"+result.grp_value+"</li>";
+					if(idxChk==0){
+						list_html += "<li data-role=\"list-divider\"  role=\"heading\" class=\"ui-li ui-li-divider ui-btn ui-bar-b ui-corner-top ui-btn-up-undefined\" >"+result.grp_value+"</li>";
+					}
+					else{
+						list_html += "<li data-role=\"list-divider\"  role=\"heading\" class=\"ui-li ui-li-divider ui-btn ui-bar-b ui-btn-up-undefined\" >"+result.grp_value+"</li>";
+					}
+					idxChk++;
 				}
-				list_html += "<li data-theme=\"d\" class=\"ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-d\">" +
+				list_html += "<li class=\"ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-d\">" +
 							 "<div class=\"ui-btn-inner ui-li\"><div class=\"ui-btn-text\">" +
 				             "<a href=\"javascript\:getTypeDetail('" + i + "')\"  class=\"ui-link-inherit\">"+ result.org_nm +"</a>" +
 				             "</div><span class=\"ui-icon ui-icon-arrow-r ui-icon-shadow\"></span></div></li>";
 							
 			}
-			list_html = "<ul data-role=\"listview\" data-inset=\"true\" data-theme=\"d\" class=\"ui-listview ui-listview-inset ui-corner-all ui-shadow\" >" +
+			list_html = "<ul data-role=\"listview\" data-inset=\"true\" class=\"ui-listview ui-listview-inset ui-corner-all ui-shadow\" >" +
 						list_html +
 						"</ul>";
 			$('#typ_list').html(list_html);
@@ -101,64 +104,15 @@ function getTypeList(type){
 function getTypeDetail(idx){
 //	alert(resultList[idx].org_nm);
 	
-	$('#d_org_nm').html(resultList[idx].org_nm);
-	$.mobile.changePage("#org_detail" ) ;
-}
-
-function fn_egov_initl_mobilephotolist(){
-	$.getJSON(contextPath + "/mbl/com/mpa/selectMobilePhotoList.mdo", function(json) {
-		var list_html = "";
-		var detail_html = "";
-		resultList = json;
-
-		totcnt = json.length;
-		
-		for (var i = 0; i < totcnt; i++) {
-			var result = resultList[i];
-
-			list_html += "<a href=\"javascript\:fn_photodetail('" + i + "')\">";
-			list_html += "<li><img alt=\"" + result.photoSj +"\" title=\""+ result.photoSj +"\" src='" + contextPath + "/cmm/fms/getImage.do?atchFileId=" + result.atchFileId + "&fileSn=0' /></li>";
-			list_html += "</a>";
-
-		}
-		detail_html += "<li>";
-		detail_html += '<img id="imgSrc" src="" title="" alt=""/>';
-		detail_html += "</li>";
-							
-		$('div[id="photoList"] ul[data-role="listview"]').html(list_html);
-		$('div[id="detailview"]').html(detail_html);
-
-		$('div[id="photoDetail"]').bind('swipeleft ', (function() {
-			imgNum++;
-			if (imgNum > totcnt-1){
-			    imgNum = 0;
-			}
-			// id 값으로 이미지를 바꿈
-			$('#imgSrc').attr('src', contextPath + '/cmm/fms/getImage.do?atchFileId=' + resultList[imgNum].atchFileId + '&fileSn=0');
-			$('#imgSrc').attr('title', resultList[imgNum].photoSj);
-			$('#imgSrc').attr('alt', resultList[imgNum].photoSj);
-        }));
-        
-		$('div[id="photoDetail"]').bind('swiperight', (function() {
-			imgNum--;
-			if (imgNum < 0){
-				imgNum = totcnt-1;
-			} 
-			$('#imgSrc').attr('src', contextPath + '/cmm/fms/getImage.do?atchFileId=' + resultList[imgNum].atchFileId + "&fileSn=0");
-			$('#imgSrc').attr('title', resultList[imgNum].photoSj);
-			$('#imgSrc').attr('alt', resultList[imgNum].photoSj);
-        }));
-		
-		$('div[id="photoDetail"]').bind('tap', (function() {
-			imgNum++;
-			if (imgNum > totcnt-1){
-			    imgNum = 0;
-			}
-			// id 값으로 이미지를 바꿈
-			$('#imgSrc').attr('src', contextPath + '/cmm/fms/getImage.do?atchFileId=' + resultList[imgNum].atchFileId + '&fileSn=0');
-			$('#imgSrc').attr('title', resultList[imgNum].photoSj);
-			$('#imgSrc').attr('alt', resultList[imgNum].photoSj);
-        }));
-		
-	});
+	$('#dvOrgNm').html("<h3>"+resultList[idx].org_nm+"</h3>");
+	$('#fOrgAddr').attr('value', resultList[idx].org_addr);
+	$('#fOrgTel').attr('value', resultList[idx].org_tel);
+	$('#fOrgUrl').attr('href', resultList[idx].org_url);
+	$('#dvOrgUrlIn').html(resultList[idx].org_url);
+	$('#fOrgCapa').attr('value', resultList[idx].org_capa);
+	$('#fOrgDetail').attr('value', resultList[idx].org_detail);
+	$('#fOrgDesc').attr('value', resultList[idx].org_desc);
+	$('#btnOrgCall').attr('href', "tel:"+resultList[idx].org_tel);
+	
+	$.mobile.changePage("#org_detail","slide",true,true) ;
 }
